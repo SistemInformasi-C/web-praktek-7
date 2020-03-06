5fc96b125b77ea2fc1a77a797d094ed071310ba4
LABKOM <syaaap69@gmail.com>
2020-03-06 10:04:19 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xss</title>
+</head>
+<body>
+<form action="" method="post">
+    <textarea name="komentar" id="komentar" cols="30" rows="10"></textarea>
+    <button>Kirim</button>
+    </form>
+</body>
+</html>
